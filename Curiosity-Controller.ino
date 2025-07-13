3e70d547ea10f7c7e3e55db03e50737d11c170d7
Joshua Brown <38842096+reallynotburner@users.noreply.github.com>
2025-07-13 14:42:01 -0400

    init commit

---
+++ Curiosity-Controller.ino
@@ -0,0 +1,123 @@
+/*********
+  Curiosity Controller
+  Drivable scale model of the Mars Science Laboratory, AKA, "Curiosity"
+  Based on code examples by:
+  Rui Santos & Sara Santos - Random Nerd Tutorials
+  Complete instructions at https://RandomNerdTutorials.com/esp32-websocket-server-sensor/
+
+  reallynotburner@gmail.com
+  Joshua Brown 2025 
+
+  Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files.
+  The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+*********/
+#include <Arduino.h>
+#include <WiFi.h>
+#include <AsyncTCP.h>
+#include <ESPAsyncWebServer.h>
+#include "LittleFS.h"
+#include <Arduino_JSON.h>
+
+// Replace with your network credentials
+const char* ssid = "curiosity";
+const char* password = "callyourmom";
+
+// Create AsyncWebServer object on port 80
+AsyncWebServer server(80);
+
+// Create a WebSocket object
+AsyncWebSocket ws("/ws");
+
+// Json Variable to Hold Sensor Readings
+JSONVar readings;
+
+// Timer variables
+unsigned long lastTime = 0;
+unsigned long timerDelay = 10000;
+
+
+// Get Sensor Readings and return JSON object
+String getSensorReadings(){
+  readings["temperature"] = String("hot!");
+  readings["humidity"] =  String("moist!");
+  readings["pressure"] = String("vacuous!");
+  String jsonString = JSON.stringify(readings);
+  return jsonString;
+}
+
+// Initialize LittleFS
+void initLittleFS() {
+  if (!LittleFS.begin(true)) {
+    Serial.println("An error has occurred while mounting LittleFS");
+  }
+  Serial.println("LittleFS mounted successfully");
+}
+
+
+void notifyClients(String sensorReadings) {
+  ws.textAll(sensorReadings);
+}
+
+void handleWebSocketMessage(void *arg, uint8_t *data, size_t len) {
+  AwsFrameInfo *info = (AwsFrameInfo*)arg;
+  if (info->final && info->index == 0 && info->len == len && info->opcode == WS_TEXT) {
+    String sensorReadings = getSensorReadings();
+    Serial.print(sensorReadings);
+    notifyClients(sensorReadings);
+  }
+}
+
+void onEvent(AsyncWebSocket *server, AsyncWebSocketClient *client, AwsEventType type, void *arg, uint8_t *data, size_t len) {
+  switch (type) {
+    case WS_EVT_CONNECT:
+      Serial.printf("WebSocket client #%u connected from %s\n", client->id(), client->remoteIP().toString().c_str());
+      break;
+    case WS_EVT_DISCONNECT:
+      Serial.printf("WebSocket client #%u disconnected\n", client->id());
+      break;
+    case WS_EVT_DATA:
+      handleWebSocketMessage(arg, data, len);
+      break;
+    case WS_EVT_PONG:
+    case WS_EVT_ERROR:
+      break;
+  }
+}
+
+void initWebSocket() {
+  ws.onEvent(onEvent);
+  server.addHandler(&ws);
+}
+
+void setup() {
+  Serial.begin(115200);
+  
+  WiFi.softAP(ssid, password);
+
+  initLittleFS();
+  initWebSocket();
+
+  // Web Server Root URL
+  server.on("/", HTTP_GET, [](AsyncWebServerRequest *request) {
+    request->send(LittleFS, "/index.html", "text/html");
+  });
+
+  server.serveStatic("/", LittleFS, "/");
+
+  // Start server
+  server.begin();
+}
+
+void loop() {
+  if ((millis() - lastTime) > timerDelay) {
+    String sensorReadings = getSensorReadings();
+    Serial.print(sensorReadings);
+    notifyClients(sensorReadings);
+    lastTime = millis();
+
+    IPAddress IP = WiFi.softAPIP();
+    Serial.print("AP IP address: ");
+    Serial.println(IP);
+  }
+  ws.cleanupClients();
+}
---
+++ README.md
@@ -0,0 +1,21 @@
+# Curiosity-Controller
+
+## Goal
+###
+I want to drive around a highly functional model of the Mars Science Laboratory (MSL), named [Curiosity](https://www.jpl.nasa.gov/missions/mars-science-laboratory-curiosity-rover-msl/).  It will have working rocker-bogie suspension, and 4 steerable motors so it can spin in place without skid steering like a tank.  I'm making it as small as I can using [Sanyo-style micro gearmotors](https://www.amazon.com/gp/aw/d/B07FVMVGM3/?_encoding=UTF8), which are inexpensive, strong, and fairly small.  The motors have to fit inside the wheel hubs and the steering mechanism without altering the proportions of the model too much for my taste.  Therefore the smallest I can make it is 1/6 scale, I could go smaller but it would mean more expesnive smaller motors.
+
+## Hardware
+### Controller
+[ESP32 Devkit](https://www.microcenter.com/product/613822/inland-esp32-wroom-32d-module) as a central controller has many input-output pins, nearly all of which can do motor controlling functions like PWM.  This is vital when it needs 10 independent motors just to drive around.  The ESP32 also has WiFi and Bluetooth built in, providing an inexpensive method of remote control.
+
+### Motors
+[Sanyo-style micro gearmotors](https://www.amazon.com/gp/aw/d/B07FVMVGM3/?_encoding=UTF8) provide the traction and steering functions of the model.  They may even provide animation of the [Mast Camera](https://science.nasa.gov/mission/msl-curiosity/science-instruments/#h-mastcam) and Robotic Arm, but that is a project more complex than just driving the model around.
+
+### Motor Driver
+[DRV8833 H-Bridges](https://www.amazon.com/VKLSVAN-DRV8833-h-Bridge-Arduino-Microcontroller/dp/B0DQGQ1V1C) provide control of 2 motors per module, so there will be 5 of these on the model for driving and steering,
+
+### Hex Inverters
+[Texas Instruments low-voltage 6 channel inverters](https://www.mouser.com/ProductDetail/Texas-Instruments/SN74LVC04APWRG3?qs=EuM%2FBx4ov4R3TYLIBa%252BMkA%3D%3D) are used to reduce the number of ESP32 pins I need to control my motor drivers.
+
+### 10k Potentiometers
+[Potentiometers](https://www.amazon.com/Taiss-Potentiometer-Variable-Resistors-Terminals/dp/B09XDR799P) are often used as volume controls.  I'm using them as a position sensor for the steerable wheels.  They convert an angle of the steering bracket into a voltage, which can be read by the ESP32, which then controls the steering motor that can move the bracket to the desired position.
